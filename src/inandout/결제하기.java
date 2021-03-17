dd450a14100fbded3cc93e193ed15f7a0c5fb047
finero-d <finero91@gmail.com>
2021-03-17 22:24:15 +0900

    지선씨 페이지 조건문 수정 커밋

--- src/inandout/결제하기.java
+++ src/inandout/결제하기.java
@@ -95,47 +95,47 @@ public class 결제하기 extends JPanel {
                 int price=0;
                 
                 
-//            for(int i=0;i<결제하기.or.length;i++) {
-//              if(결제하기.orderlist[i].getText()!=null) {
-//              if(orderlist[i].getText()=="블랙 밀크티") {
-//                black=Integer.parseInt(결제하기.count[i].getText());
-//                System.out.println(black);
-//             }
-//             else if(결제하기.orderlist[i].getText()=="타로 밀크티") {
-//                taro=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="우롱 밀크티") {
-//                oolong=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="딸기 쥬얼리 밀크티") {
-//                berry=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="얼그레이 밀크티") {
-//                earlgrey=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="자스민 밀크티") {
-//                jasmin=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="망고 스무디") {
-//                mango=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="청포도 스무디") {
-//                grape=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="제주 그린티 스무디") {
-//                greentea=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="치즈폼 딥초코 스무디") {
-//                cheese=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="딸기 요구르트 크러쉬") {
-//                yogurt=Integer.parseInt(결제하기.count[i].getText());
-//             }
-//             else if(결제하기.orderlist[i].getText()=="초코 쿠앤크 스무디") {
-//                cookie=Integer.parseInt(결제하기.count[i].getText());
-//             }          
-//          }         
-//       }
+            for(int i=0;i<결제하기.or.length;i++) {
+              if(결제하기.orderlist[i].getText()!=null) {
+              if(orderlist[i].getText().equals("블랙 밀크티")) {
+                black=Integer.parseInt(결제하기.count[i].getText());
+                System.out.println(black);
+             }
+             else if(결제하기.orderlist[i].getText()==("타로 밀크티")) {
+                taro=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("우롱 밀크티")) {
+                oolong=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("딸기 쥬얼리 밀크티")) {
+                berry=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("얼그레이 밀크티")) {
+                earlgrey=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("자스민 밀크티")) {
+                jasmin=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("망고 스무디")) {
+                mango=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("청포도 스무디")) {
+                grape=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("제주 그린티 스무디")) {
+                greentea=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("치즈폼 딥초코 스무디")) {
+                cheese=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("딸기 요구르트 크러쉬")) {
+                yogurt=Integer.parseInt(결제하기.count[i].getText());
+             }
+             else if(결제하기.orderlist[i].getText()==("초코 쿠앤크 스무디")) {
+                cookie=Integer.parseInt(결제하기.count[i].getText());
+             }          
+          }         
+       }
        if(결제하기.money.getText()!=null) {
           price=Integer.parseInt(결제하기.money.getText());
        }
